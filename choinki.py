0a6807b8e4783ad0e27d5d33f24e91570e59b1c6
notnamenot <33725887+notnamenot@users.noreply.github.com>
2018-01-06 16:48:36 +0100

    Update choinki.py

--- choinki.py
+++ choinki.py
@@ -9,3 +9,13 @@ for i in range(2, n+1):
     for j in range(1, i+1):
         print("x"*j)
         
+#cała choinka 
+
+n=int(input("Podaj liczbę n\n"))         #ostatnia linia zawiera 2n +1 znaków
+if n<1:
+    print("n musi byc wieksze niz 1")
+    n=int(input("Podaj liczbę n\n"))
+
+for i in range(0, n):
+    for i in range(0, (n-1)+i):
+        print(" "*(2*n-i)+"x"*2*i+"x")
