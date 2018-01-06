bf94562d30d0a2d4862a67e533ffd4b78feddd70
notnamenot <33725887+notnamenot@users.noreply.github.com>
2018-01-06 20:16:43 +0100

    Update dec_to_bin.py

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -8,3 +8,20 @@ def dec_to_bin(d):
     
 d=int(input("Podaj liczbę w systemie dziesiętnym:\n"))
 print("Ta liczba w systemie binarnym wynosi", dec_to_bin(d))
+
+
+
+def dec_to_random(d,sys):     #decimal, system
+    r=""
+    
+    while d>0:
+        r+=str(d%sys)
+        d//=sys
+    r=r[::-1]
+    return r        #str
+
+d=int(input("Podaj liczbę w systemie dziesiątkowycm:\n"))   #int
+sys=int(input("Podaj na jaki system przeliczać:\n"))        #int
+
+print("Liczba w nowym systemie wynosi:\n", dec_to_random(d,sys))
+
