d2762564723cffe828084857bc2922cb58a5cac3
notnamenot <33725887+notnamenot@users.noreply.github.com>
2018-01-20 20:59:26 +0100

    Update 1.4,5choinki.py

--- 1.4,5choinki.py
+++ 1.4,5choinki.py
@@ -19,3 +19,12 @@ if n<1:
 for i in range(0, n):
     for i in range(0, (n-1)+i):
         print(" "*(2*n-i)+"x"*2*i+"x")
+
+        
+# program rysuje choinke o podstawie 2k+1
+n=int(input("Podaj liczbe n>=1"))
+for i in range (1,n+1):
+    for k in range (0,i+1):
+        print(" "*(n-k+1),end="")
+        print("x"*(2*k+1))
+            
