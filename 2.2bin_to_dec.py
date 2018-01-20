3fa7a118a2d53a64baed1cd0a0c1218d82752a03
notnamenot <33725887+notnamenot@users.noreply.github.com>
2018-01-20 17:58:18 +0100

    Update 2.2bin_to_dec.py

--- 2.2bin_to_dec.py
+++ 2.2bin_to_dec.py
@@ -3,7 +3,7 @@ b=input("Podaj liczbę w sysytemie binarnym:\n")
 def bin_to_dec(b):
   d=0
   for i in range(len(b)):
-    d=d+2**(len(b)-i-1)*int(b[i])
+    d+=2**(len(b)-i-1)*int(b[i])
   return d
 
 print("Ta liczba w systemie dziesiętnym to", bin_to_dec(b))  
