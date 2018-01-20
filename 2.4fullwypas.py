a877ca69c4c77753d2a587f7a1babfe3004752c8
notnamenot <33725887+notnamenot@users.noreply.github.com>
2018-01-20 20:28:11 +0100

    Update 2.4fullwypas.py

--- 2.4fullwypas.py
+++ 2.4fullwypas.py
@@ -1,4 +1,43 @@
+def sys_to_dec(b,sys):
+    d=0
+    for i in range(len(b)):
+        if ord(b[i])>=48 and ord(b[i])<=57:
+            d+=(sys**(len(b)-i-1))*(ord(b[i])-48)
+        elif ord(b[i])>=65:
+            d+=(sys**(len(b)-i-1))*(ord(b[i])-55)
+    return d
 
+def dec_to_sys(dec,sys1):
+    r=""
+    while dec>0:
+        if dec%sys1>=0 and dec%sys1<=9:
+            r+=str(dec%sys1)
+        elif dec%sys1>9:
+            r+=chr(dec%sys1+55)
+        dec//=sys1
+    r=r[::-1]
+    return r
+        
+
+
+
+b=input("Podaj liczbę do konwersji:\n")
+sys=int(input("Podaj system z konwersji:\n"))
+
+dec=sys_to_dec(b,sys)
+#print("Liczba w dziesietnym:\n", dec)
+
+sys1=int(input("System na konwersje:\n"))
+s=dec_to_sys(dec, sys1)
+
+print("Liczba w nowym systemie:\n", s)    
+
+
+
+
+
+
+"""
 l=input("Podaj liczbę do konwersji:\n")    
 sys=int(input("Podaj system z konwersji:\n"))
 sys1=int(input("Podaj system do konwersji:\n"))     
@@ -21,3 +60,4 @@ def dec_to_sys(a,sys1):
     return r
           
 print("Liczba po konwersji to:", dec_to_sys(sys_to_dec(l,sys) ,sys1))
+"""
